563d2cb5ccb967f45eb4e14cf90529ebbaff706b
Jay <35229@ma-web.nl>
2022-10-25 23:26:20 +0200

    inleiding aangemaakt

--- helloyou.py
+++ helloyou.py
@@ -1,5 +1,23 @@
 "# M1BO-Hello_You" 
-print("test")
+import time 
+
+print("Jij bent een barbaar met de meest sterksten krachten maar die krachten heb je niet zomaar gekregen, je hebt ze gekregen van een tovenaar genaamd Skeletor.")
+time.sleep(3.0)
+print("De tovenaar geeft je dagelijkse questen op om je krachten te behouden.")
+time.sleep(3.0)
+print("Doe je dat niet dan verlies je je krachten en verwoest de tovenaar je thuisland.")
+time.sleep(3.0)
+print("De tovenaar geeft je drie keuzen om drie verschillenden questen te doen. Je kan kiezen tussen de CAVES, de DUNGOUN of de FOREST.")
+print("Kiez uit caves, dungoun, forest")
 
 
-print("babarian")
+choice = input() 
+
+if choice == 'caves':
+    def caves 
+elif choice == 'dungoun':
+    def dungoun 
+elif choice == 'forest':
+    def forest
+else:
+    print("kiez uit dungoun, caves of forest")
