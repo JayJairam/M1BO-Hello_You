a7de6d9d3c9b9b5f1bda08dc0ff6da7028130577
Jay <35229@ma-web.nl>
2022-10-26 12:36:00 +0200

    aan def caves gewerkt

--- helloyou.py
+++ helloyou.py
@@ -1,23 +1,102 @@
 "# M1BO-Hello_You" 
 import time 
 
-print("Jij bent een barbaar met de meest sterksten krachten maar die krachten heb je niet zomaar gekregen, je hebt ze gekregen van een tovenaar genaamd Skeletor.")
-time.sleep(3.0)
-print("De tovenaar geeft je dagelijkse questen op om je krachten te behouden.")
-time.sleep(3.0)
-print("Doe je dat niet dan verlies je je krachten en verwoest de tovenaar je thuisland.")
-time.sleep(3.0)
-print("De tovenaar geeft je drie keuzen om drie verschillenden questen te doen. Je kan kiezen tussen de CAVES, de DUNGOUN of de FOREST.")
-print("Kiez uit caves, dungoun, forest")
+
+def dead():
+    print("""
+                            ____________
+                      .~      ,   . ~.
+                     /                \
+                    /      /~\/~\   ,  \
+                   |   .   \    /   '   |
+                   |         \/         |
+          XX       |  /~~\        /~~\  |       XX
+        XX  X      | |  o  \    /  o  | |      X  XX
+      XX     X     |  \____/    \____/  |     X     XX
+ XXXXX     XX      \         /\        ,/      XX     XXXXX
+X        XX%;;@      \      /  \     ,/      @%%;XX        X
+X       X  @%%;;@     |           '  |     @%%;;@  X       X
+X      X     @%%;;@   |. ` ; ; ; ;  ,|   @%%;;@     X      X
+ X    X        @%%;;@                  @%%;;@        X    X
+  X   X          @%%;;@              @%%;;@          X   X
+   X  X            @%%;;@          @%%;;@            X  X
+    XX X             @%%;;@      @%%;;@             X XX
+      XXX              @%%;;@  @%%;;@              XXX
+                         @%%;;%%;;@
+                           @%%;;@
+                         @%%;;@..@@
+                          @@@  @@@
+""")
+    time.sleep(3.0)
+    print("you died!!!")
+
+def award1():
+    print("Je hebt het wezen verslagen")
+    time.sleep(2.7)
 
 
-choice = input() 
+def intro():
+    print("Jij bent een barbaar met de meest sterksten krachten maar die krachten heb je niet zomaar gekregen, je hebt ze gekregen van een tovenaar genaamd Skeletor.")
+    time.sleep(3.0)
+    print("De tovenaar geeft je dagelijkse questen op om je krachten te behouden.")
+    time.sleep(3.0)
+    print("Doe je dat niet dan verlies je je krachten en verwoest de tovenaar je thuisland.")
+    time.sleep(3.0)
+    print("De tovenaar geeft je drie keuzen om drie verschillenden questen te doen. Je kan kiezen tussen de CAVES, de DUNGOUN of de FOREST.")
+    print("Kiez uit caves, dungoun, forest")
 
-if choice == 'caves':
-    def caves 
-elif choice == 'dungoun':
-    def dungoun 
-elif choice == 'forest':
-    def forest
-else:
-    print("kiez uit dungoun, caves of forest")
+
+def caves():
+    print("Je komt bij de gevaarlijke Caves aan genaamd dodelijke grotten.")
+    time.sleep(2.7)
+    print("Daar moest je diamonds halen of het hoofd van de monster voor de tovenaar.")
+    time.sleep(2.7)
+    print(". Je ziet de daimonds maar daarnaast zie je een grote monster genaamd roadhog.")
+    print("wat ga je doen, ga je voor de daimonds of monster")
+
+    for i in range(5):
+        choice = input()
+        if choice == 'daimonds':
+            dead()
+            break
+        elif choice == 'monster':
+            print("hoera je hebt de monster verslagen")
+            time.sleep(2.8)
+            print("pak je het hoofd van de moster of pak je de daimonds")
+        else:
+            print("kiez uit hoofd van moster of daimonds")
+
+
+
+
+
+
+
+
+
+
+
+
+def dungoun():
+    print("test")
+
+def forest():
+    print("test")
+
+
+intro()
+
+
+for i in range(5):
+    choice = input() 
+    if choice == 'caves':
+        caves()
+        break
+    elif choice == 'dungoun':
+        dungoun()
+        break
+    elif choice == 'forest':
+        forest()
+        break
+    else:
+        print("kiez uit dungoun, caves of forest")
