b783a42b69a5d4036c794bb0ef5838b3f2316665
Jay <35229@ma-web.nl>
2022-10-26 14:34:11 +0200

    caves def upgedate

--- helloyou.py
+++ helloyou.py
@@ -30,9 +30,36 @@ X      X     @%%;;@   |. ` ; ; ; ;  ,|   @%%;;@     X      X
     time.sleep(3.0)
     print("you died!!!")
 
+
+
+
+
+
 def award1():
     print("Je hebt het wezen verslagen")
     time.sleep(2.7)
+    print("Je bent de meest rijkste barbaar ooit.")
+    print("""
+    ⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀
+⢠⣤⣤⣤⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣄⣤⣤⣠
+⢸⠀⡶⠶⠾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡷⠶⠶⡆⡼
+⠈⡇⢷⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠇⠀⢸⢁⡗
+⠀⢹⡘⡆⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠀⢀⡏⡼⠀
+⠀⠀⢳⡙⣆⠈⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⢀⠞⡼⠁⠀
+⠀⠀⠀⠙⣌⠳⣼⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣞⡴⣫⠞⠀⠀⠀
+⠀⠀⠀⠀⠈⠓⢮⣻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⣩⠞⠉⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠉⠛⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠋⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⢤⣀⠀⠀⠀⠀⢀⣠⠖⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⡇⢸⡏⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠖⠒⠓⠚⠓⠒⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣀⣠⣞⣉⣉⣉⣉⣉⣉⣉⣉⣉⣉⣙⣆⣀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠓⠲⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠖⠃⠀⠀⠀⠀
+""")
+
+
 
 
 def intro():
@@ -45,6 +72,21 @@ def intro():
     print("De tovenaar geeft je drie keuzen om drie verschillenden questen te doen. Je kan kiezen tussen de CAVES, de DUNGOUN of de FOREST.")
     print("Kiez uit caves, dungoun, forest")
 
+def caves2():
+    print("Je kunt kieze uit om de daimonds mee te nemen of het hoofd mee te nemen.")
+    for i in range(5):
+        choice = input()
+        if choice == 'daimonds':
+            award1()
+        elif choice = 'hoofd meenemen':
+            award2()
+        else:
+            print("kiez uit daimonds of hoofd meenemen")
+        
+
+
+
+
 
 def caves():
     print("Je komt bij de gevaarlijke Caves aan genaamd dodelijke grotten.")
@@ -53,7 +95,7 @@ def caves():
     time.sleep(2.7)
     print(". Je ziet de daimonds maar daarnaast zie je een grote monster genaamd roadhog.")
     print("wat ga je doen, ga je voor de daimonds of monster")
-
+    
     for i in range(5):
         choice = input()
         if choice == 'daimonds':
@@ -62,10 +104,9 @@ def caves():
         elif choice == 'monster':
             print("hoera je hebt de monster verslagen")
             time.sleep(2.8)
-            print("pak je het hoofd van de moster of pak je de daimonds")
+            caves2()
         else:
             print("kiez uit hoofd van moster of daimonds")
-
 
 
 
